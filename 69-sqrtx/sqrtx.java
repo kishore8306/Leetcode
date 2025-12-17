1a97c78569df39dfb92622cffecfcb2b0a77fc31
Kishore A <kishoreak0820@gmail.com>
2025-12-17 19:14:09 +0530

    Time: 23 ms (5.63%) | Memory: 42.7 MB (10.55%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2) return x;
+
+        int i = 1;
+        while((long)i * i <= x){
+            i++;
+        }
+        return i-1;
+    }
+}
