8f4ed39f9d803af9489d1025d6ba97c98108436a
Kishore A <kishoreak0820@gmail.com>
2026-01-16 22:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (61.51%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) return false;
+
+        Map<Character, String> map = new HashMap<>();
+        Set<String> used = new HashSet<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            if (!map.containsKey(c)) {
+                if (used.contains(words[i])) return false;
+                map.put(c, words[i]);
+                used.add(words[i]);
+            } else if (!map.get(c).equals(words[i])) return false;
+        }
+        return true;
+    }
+}
